d5c9e63ca9b5ab21e7052f185fb5238917e06570
Ian Humprey <ian.humprey09@gmail.com>
2026-01-08 16:27:34 +0800

    Added Modal and button for the serials modal

--- resources/js/pages/serials.tsx
+++ resources/js/pages/serials.tsx
@@ -1,5 +1,8 @@
 import AppLayout from '@/layouts/app-layout';
 import { Head } from '@inertiajs/react';
+import Modal from '@/components/modal';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 const breadcrumbs = [
     {
@@ -7,11 +10,16 @@ const breadcrumbs = [
         href: '/serials',
     }
 ]
+
 export default function Serials() {
+
+    const [isOpen, setIsModalOpen] = useState(false);
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Serials" />
-            <div className='m-5'>
+            <div className='m-5 relative'>
+                <Button className="absolute top-0 right-0" onClick={() => setIsModalOpen(true)}>Add Serial</Button>
                 <table className='w-1/3'>
                     <thead>
                         <tr>
@@ -30,6 +38,19 @@ export default function Serials() {
                         </tr>
                     </tbody>
                 </table>
+                <Modal isOpen={isOpen} onClose={() => setIsModalOpen(false)}>
+                    <div className='relative'>
+                        <Button onClick={() => setIsModalOpen(false)} className='absolute right-0'>X</Button>
+                        <h3 className='text-center'>Serial</h3>
+                        <form className='block' action="">
+                            <legend>Serial Number:</legend>
+                            <input type="text" name='serial' placeholder='Serial Number' className='w-full'/>
+                            <legend>Date</legend>
+                            <input type="date" name='datebought' className='w-full' />
+                            <Button type='submit' className='mt-3 justify-center'>Add Serial</Button>
+                        </form>
+                    </div>
+                </Modal>
             </div>
         </AppLayout>
     )
